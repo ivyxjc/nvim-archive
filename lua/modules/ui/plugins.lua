ff1027b5016c47b5d474f332b35c5b021884f515
ivyxjc <ivyxjc1994@hotmail.com>
2021-04-06 19:33:30 +0800

    add got signs

--- lua/modules/ui/plugins.lua
+++ lua/modules/ui/plugins.lua
@@ -52,12 +52,66 @@ ui['romgrk/barbar.nvim'] = {
     requires = {'kyazdani42/nvim-web-devicons'}
 }
 
-ui['vim-airline/vim-airline'] = {
+ui['vim-airline/vim-airline'] = {}
 
-}
+ui['vim-airline/vim-airline-themes'] = {}
 
-ui['vim-airline/vim-airline-themes'] = {
-    
+function config._gitsigns()
+    if not packer_plugins['plenary.nvim'].loaded then
+        vim.cmd [[packadd plenary.nvim]]
+    end
+    require('gitsigns').setup {
+        signs = {
+            add = {
+                hl = 'GitGutterAdd',
+                text = '▋'
+            },
+            change = {
+                hl = 'GitGutterChange',
+                text = '▋'
+            },
+            delete = {
+                hl = 'GitGutterDelete',
+                text = '▋'
+            },
+            topdelete = {
+                hl = 'GitGutterDeleteChange',
+                text = '▔'
+            },
+            changedelete = {
+                hl = 'GitGutterChange',
+                text = '▎'
+            }
+        },
+        keymaps = {
+            -- Default keymap options
+            noremap = true,
+            buffer = true,
+
+            ['n ]g'] = {
+                expr = true,
+                "&diff ? ']g' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>'"
+            },
+            ['n [g'] = {
+                expr = true,
+                "&diff ? '[g' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"
+            },
+
+            -- ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+            -- ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+            -- ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+            -- ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+            -- ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+
+            -- Text objects
+            ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
+            ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>'
+        }
+    }
+end
+ui['lewis6991/gitsigns.nvim'] = {
+    config = config._gitsigns,
+    requires = {'nvim-lua/plenary.nvim'}
 }
 
 return ui
