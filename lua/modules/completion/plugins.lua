26120cc4a14e1f701c7c88bf8bc2cb16b126ae94
ivyxjc <ivyxjc1994@hotmail.com>
2021-04-08 11:28:33 +0800

    editor lang config

--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -20,4 +20,19 @@ completion['hrsh7th/vim-vsnip'] = {
     config = conf.vim_vsnip
 }
 
+completion['nvim-telescope/telescope.nvim'] = {
+    cmd = 'Telescope',
+    config = conf.telescope,
+    requires = {{
+        'nvim-lua/popup.nvim',
+        opt = true
+    }, {
+        'nvim-lua/plenary.nvim',
+        opt = true
+    }, {
+        'nvim-telescope/telescope-fzy-native.nvim',
+        opt = true
+    }}
+}
+
 return completion
